72f3267513529c433ce16a9b8fc15005228db683
Changwook Doh <changwook.doh@gmail.com>
2025-12-07 19:05:21 +0900

    Initialize Spring Boot Project

---
+++ src/main/java/com/hello/pgproxy/PgproxyApplication.java
@@ -0,0 +1,11 @@
+package com.hello.pgproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgproxyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PgproxyApplication.class, args);
+    }
+}
