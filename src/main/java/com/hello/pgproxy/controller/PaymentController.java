1704303888a72e87625caf62ef38e8459a600d4e
Changwook Doh <changwook.doh@gmail.com>
2025-12-12 00:28:41 +0900

    Enable swagger

--- src/main/java/com/hello/pgproxy/controller/PaymentController.java
+++ src/main/java/com/hello/pgproxy/controller/PaymentController.java
@@ -3,6 +3,11 @@ package com.hello.pgproxy.controller;
 import com.hello.pgproxy.configuration.ResponseProperties;
 import com.hello.pgproxy.model.ClientRequest;
 import com.hello.pgproxy.service.PaymentProxyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +18,27 @@ import org.springframework.web.context.request.async.DeferredResult;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Payment Processing", description = "Endpoints for handling payment requests and managing backpressure.") // 컨트롤러 전체 태그
 public class PaymentController {
     private final PaymentProxyService proxyService;
     private final ResponseProperties responseProperties;
 
+    @Operation(
+            summary = "Submit a payment request through the proxy",
+            description = "Forwards the request to the backend after computing the verification number and handling prioritization."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Payment processed successfully."
+    )
+    @ApiResponse(
+            responseCode = "400",
+            description = "Invalid request or Wrong Verification Number."
+    )
+    @ApiResponse(
+            responseCode = "503",
+            description = "Backend Payment Gateway is overloaded. Request might be queued or rejected based on priority."
+    )
     @PostMapping("/payments")
     public DeferredResult<ResponseEntity<?>> processPayment(@RequestBody ClientRequest request) {
         DeferredResult<ResponseEntity<?>> deferredResponse = createDeferredResult();
