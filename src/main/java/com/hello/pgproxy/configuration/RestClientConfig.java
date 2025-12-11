68620263933345c86802eb1f3a419816efe97bf2
Changwook Doh <changwook.doh@gmail.com>
2025-12-12 00:29:26 +0900

    Remove unused prop

--- src/main/java/com/hello/pgproxy/configuration/RestClientConfig.java
+++ src/main/java/com/hello/pgproxy/configuration/RestClientConfig.java
@@ -1,17 +1,13 @@
 package com.hello.pgproxy.configuration;
 
 import lombok.Getter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@ConfigurationProperties(prefix = "endpoints")
 @Getter
 public class RestClientConfig {
-    private String url;
-
     @Bean
     public RestTemplate backendRestTemplate() {
         return new RestTemplate();
